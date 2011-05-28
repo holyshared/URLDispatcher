1016a60d171fcb00729375544942b8380be0dc58
Noritaka Horio <holy.shared.design@gmail.com>
2011-05-28 19:06:01 +0900

    replace source.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

---
+++ Source/URLDispacher.js
@@ -0,0 +1,38 @@
+/*
+---
+name: URLDispacher
+
+description: 
+
+license: MIT-style
+
+authors:
+- Noritaka Horio
+
+requires:
+  - Core/Type
+  - Core/Function
+
+provides: [URLDispacher]
+
+...
+*/
+
+(function(){
+
+var URLDispacher = this.URLDispacher = function(){
+}
+
+URLDispacher.implement({
+
+	_handlers :[],
+
+	register: function(paturn, handler){
+	},
+
+	dispatch: function(url){
+	}
+
+});
+
+}());
