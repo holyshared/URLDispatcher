9b5d444bb8d1c7824c809c51ea108670205d6a0c
Noritaka Horio <holy.shared.design@gmail.com>
2011-05-29 21:36:57 +0900

    demo fixed.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

---
+++ js/builder.js
@@ -0,0 +1,31 @@
+(function(doc){
+
+	var Builder = this.Builder = {};
+
+	var elements = ['h1', 'h2', 'p', 'dl', 'dt', 'dd', 'ul', 'li'];
+	for (var i = 0; i < elements.length; i++){
+		var key = elements[i];
+		Builder[key] = build(key);
+	};
+	
+	function build(name){
+		var key = name;
+		return function(value){
+			return element(key, value);
+		}
+	}
+	
+	function text(value){
+		return doc.createTextNode(value);
+	}
+	
+	function element(element, value){
+		var node = doc.createElement(element);
+		if (value){
+			var nodeText = text(value); 
+			node.appendChild(nodeText);
+		}
+		return node;
+	}
+
+}(document));
--- js/handlers.js
+++ js/handlers.js
@@ -1,12 +1,82 @@
-(function(){
+(function(doc, Builder){
 
-var Application = this.Application = {};
+var App = this.Application = {};
 
-Application.EventHandler = function(context){
+App.RootHandler = function(context){
+	var values = context.values;
+	var content = values.content;
+
+	content.appendChild(Builder.h2('url'));
+	content.appendChild(Builder.p(context.url));
+};
+
+App.EventHandler = function(){};
+App.EventHandler.implement({
+
+	preDispatch: function(context){
+	},
+
+	invoke: function(context){
+
+		var params = context.params;
+		var values = context.values;
+		var content = values.content;
+
+		content.appendChild(Builder.h2('url'));
+		content.appendChild(Builder.p(context.url));
+
+		if (Type.isArray(params)){
+			content.appendChild(Builder.h2('params'));
+			content.appendChild(buildUlList(params));
+		}
+
+		if (values){
+			content.appendChild(Builder.h2('values'));
+			content.appendChild(buildDlList(values));
+		}
+
+	},
+
+	postDispatch: function(context){
+	}
+
+});
+
+function buildUlList(values){
+	var ul = Builder.ul();
+	for(var i = 0; i < values.length; i++) {
+		var li = Builder.li(values[i]);
+		ul.appendChild(li);
+	}
+	return ul;
 }
 
+function buildDlList(values){
 
-Application.EventHandler = function(context){
+	var dl = Builder.dl();
+	for (var key in values){
+
+		var value = values[key];
+		switch(typeOf(value)){
+			case 'object':
+				var dd = Builder.dd('object');
+				break;
+			case 'function':
+				var dd = Builder.dd('function');
+				break;
+			case 'element':
+				var dd = Builder.dd('element');
+				break;
+			default:
+				var dd = Builder.dd(value);
+				break;
+		}
+		var dt = Builder.dt(key);
+
+		dl.appendChild(dt);
+		dl.appendChild(dd);
+	}
+	return dl;
 }
 
-}());
+}(document, Builder));
