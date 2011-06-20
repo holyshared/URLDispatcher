dbeac6a47c849da07891bc584897d7089c02adcd
Noritaka Horio <holy.shared.design@gmail.com>
2011-06-21 02:24:27 +0900

    add methods.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- Source/URLDispatcher.Dispatcher.js
+++ Source/URLDispatcher.Dispatcher.js
@@ -19,3 +19,42 @@ provides: [URLDispatcher.Dispathcer]
 
 ...
 */
+(function(dispatcher){
+
+
+dispatcher.Dispatcher = new Class({
+
+	_handlers: {},
+
+	initialize: function(){
+		this.router = new dispatcher.Router();
+	},
+
+	register: function(paturn, handler, conditions){
+		this._addHandler(paturn, handler);
+		this.router.addRoute(paturn, conditions);
+	},
+
+	dispatch: function(url, context){
+		var paturn = this.router.match(url);
+		if (!paturn) {
+			return false;
+		}
+
+		var handler = this._getHandler(paturn);
+
+
+
+	},
+
+	_addHandler: function(paturn, handler){
+		this._handlers[paturn] = handler;
+	}.protect(),
+
+	_getHandler: function(paturn){
+		return this._handlers[paturn];
+	}.protect()
+
+});
+
+}(URLDispatcher));
